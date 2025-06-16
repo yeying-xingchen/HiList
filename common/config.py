f1db3fc63ad65a08e808fc4ef3ee9e78d08707d8
Yeying-Xingchen <yeyingxingchen@yeah.net>
2025-06-16 13:28:24 +0800

    增加默认数据库配置

--- common/config.py
+++ common/config.py
@@ -38,7 +38,13 @@ def get_website_name(default="HiList"):
 def get_db_info() -> dict:
     """获取数据库配置"""
     config = _load_config()
-    db_config = config.get('database', {})
+    db_config = config.get('database', {
+        "host": "127.0.0.1",
+        "port": 3306,
+        "database": "hilist",
+        "user": "root",
+        "password": "123456"
+    })
     if not db_config:
         log.error("数据库配置缺失! 请检查配置文件")
     return db_config
